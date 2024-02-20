73e69e1330b0a7a10c105f7d3da51bd437b44d34
alhasann351 <alhasann352@gmail.com>
2024-02-20 22:46:06 +0600

    inkwell added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -13,7 +14,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        textTheme: GoogleFonts.amaranthTextTheme(),
+        textTheme: const TextTheme(
+          labelLarge: TextStyle(fontSize: 19, color: Colors.white),
+        ),
         primaryColor: const Color(0xFFFC6736),
       ),
       home: const MyHomePage(),
@@ -29,22 +32,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //snackBar
+  mySnackBar(context, msg) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          msg,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Theme.of(context).primaryColor,
+        action: SnackBarAction(
+          label: 'Dismiss',
+          textColor: Colors.white,
+          //backgroundColor: Colors.white,
+          onPressed: () {
+            ScaffoldMessenger.of(context).hideCurrentSnackBar();
+          },
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
-        title: const Text(
+        title: Text(
           'Inkwell',
-          style: TextStyle(
+          style: GoogleFonts.montserrat(
             fontSize: 25,
             fontWeight: FontWeight.bold,
-            color: Colors.white,
+            color: Colors.white, // Or your desired color
           ),
         ),
       ),
-      body: const Center(
-        child: Text('Inkwell'),
+      body: Center(
+        child: InkWell(
+          splashColor: Colors.grey,
+          onDoubleTap: () {
+            mySnackBar(context, 'Double Tap');
+          },
+          onTap: (){
+            mySnackBar(context, 'Tap');
+          },
+          onLongPress: (){
+            mySnackBar(context, 'Long Press');
+          },
+          onTapDown: (TapDownDetails details){
+            mySnackBar(context, 'Tap Down');
+          },
+          child: Container(
+            height: 60,
+            width: 250,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(15),
+              gradient: const LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                //stops: [0.5, 0.7],
+                colors: [
+                  Color(0xFF0C2D57),
+                  Color(0xFFFC6736),
+                  Color(0xFF73FF00),
+                ],
+              ),
+            ),
+            child: Text(
+              'Inkwell',
+              style: GoogleFonts.amaranth(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
